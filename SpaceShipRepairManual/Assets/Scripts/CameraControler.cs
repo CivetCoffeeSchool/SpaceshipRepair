72a1aff0103f409745bfa1bd8bd8849d7da54965
CivetCoffeeSchool <132644816+CivetCoffeeSchool@users.noreply.github.com>
2023-06-03 23:53:23 +0200

    Mapteilweise und Parallex fertig

--- SpaceShipRepairManual/Assets/Scripts/CameraControler.cs
+++ SpaceShipRepairManual/Assets/Scripts/CameraControler.cs
@@ -7,6 +7,6 @@ public class CameraControler : MonoBehaviour
     [SerializeField] private Transform player;
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, -10);
+        transform.position = new Vector3(player.position.x, player.position.y,-10);
     }
 }
